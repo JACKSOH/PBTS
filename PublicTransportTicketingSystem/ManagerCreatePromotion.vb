6424cace5f58c2787a6abe6dd3f173c123f6a33e
Tan Yisi <tanyisi-wa16@student.tarc.edu.my>
2019-04-11 13:06:43 +0800

    modify create promotion part 1

--- PublicTransportTicketingSystem/ManagerCreatePromotion.vb
+++ PublicTransportTicketingSystem/ManagerCreatePromotion.vb
@@ -1,3 +1,31 @@
 ﻿Public Class ManagerCreatePromotion
+    Friend discountRate As Double
+    Friend endDate As Date
 
+    Private Sub radFullDiscount_CheckedChanged(sender As Object, e As EventArgs) Handles radFullDiscount.CheckedChanged
+        nupCustomDiscount.Enabled = False
+    End Sub
+
+    Private Sub ManagerMenuLayoutControl1_Load(sender As Object, e As EventArgs) Handles ManagerMenuLayoutControl1.Load
+        If (radFullDiscount.Checked = True) Then
+            discountRate = 1
+        ElseIf (radCustomDiscount.Checked = True) Then
+            discountRate = Double.Parse(nupCustomDiscount.Value.ToString) / 100
+        End If
+
+        endDate = dpPromotionStartDate.Value.AddDays(Integer.Parse(nupPromotionDuration.Value.ToString))
+    End Sub
+
+    Private Sub radCustomDiscount_CheckedChanged(sender As Object, e As EventArgs) Handles radCustomDiscount.CheckedChanged
+        nupCustomDiscount.Enabled = True
+    End Sub
+
+    Private Sub btnNext_Click(sender As Object, e As EventArgs) Handles btnNext.Click
+        If (txtPromotionName.Text = "") Then
+            ErrorName.Visible = True
+        Else
+            ErrorName.Visible = False
+
+        End If
+    End Sub
 End Class
